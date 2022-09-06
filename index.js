57b865af618eef9748b4f4b0000ad9bc2332f4bc
Mirko <mirkomassa18@gmail.com>
2022-09-06 07:00:39 +0200

    deploying mangGet on Heroku

--- index.js
+++ index.js
@@ -1,23 +1,37 @@
 require('dotenv').config();
 const {Telegraf} = require('telegraf');
+//const { Composer } = require('micro-bot');
 const puppeteer = require('puppeteer');
 
 //my web scraper module
 const scrape = require('./src/swagWebScraper.js')
 
-//retrieving data from .env file
-const{TOKEN, SERVER_URL} = process.env;
 
 const bot = new Telegraf(TOKEN);
+
+/*
+//trying microbot (includes Telegraf)
+const bot = new Composer();
+*/
+
+//ngrok
+
+//retrieving data from env variables
+const{TOKEN, SERVER_URL} = process.env;
 
 //ngrok webhook
 const URI = `/webhook/${TOKEN}`;
 const webhook = SERVER_URL+URI;
 
-let anime_manga;
+
+
 bot.catch((err, ctx) => {
     console.log(`Error ${ctx.updateType}`, err)
   })
+
+let anime_manga;  
+
+
 bot.start((ctx) => {
 
     bot.telegram.sendMessage(ctx.chat.id, 'Welcome to MangGet! What are you looking for?',
@@ -187,15 +201,28 @@ bot.on('message', (ctx) => {
                         buttons
                     }
                 });
-            }//fine else
+            }
         }
 
         anime();
     }
 
+})
+
+//***connection***
+
+bot.launch({
+    webhook: {
+        domain:webhook,
+        port: 5000
+    }
+})
 
 
-
+/*
+//***heroku connection using microbot***
+module.exports = bot;
+*/
 
     //The code below doesn't work because cheerio doesn't simulate the browser,
     //all that I get from that page scraping are variable names. 
@@ -208,26 +235,5 @@ bot.on('message', (ctx) => {
     //       console.log($(link).text() + ':\n  ' + $(link).attr('href'));
     //     });
     // });
-})
 
 
-bot.launch({
-    webhook: {
-        domain:webhook,
-        port: 5000
-    }
-})
-
-//manga
-// https://mangasee123.com/search/?name=
-
-//anime
-// anime8.ru
-// kickassanime.rs
-// animekaizoku.com
-// gogoanime.in
-// justdubs.org
-// vidstreaming.io
-// animeshow.tv
-// animerush.tv
-// twistmoe.net
